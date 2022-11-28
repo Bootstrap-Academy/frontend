3a5ff5f1672c29232642dc30bcd78f06b73e649b
Rumman-Fahim <rumman.fahim97@gmail.com>
2022-11-28 11:13:14 +0500

    refined oAuth

---
+++ pages/oauth/callback.vue
@@ -0,0 +1,100 @@
+<!--
+❌ Responsive UI
+✅ Page Title
+❌ Translation
+❌ Animation
+❌ middleware
+
+❌ Tested on chrome
+❌ Tested on firefox
+❌ Tested on safari
+❌ Tested on android mobile
+❌ Tested on apple mobile
+
+❌ Handle loading if data already exists
+❌ Handle loading if data is empty
+❌ Display data
+❌ Handle empty state
+
+❌ Recaptcha
+❌ Api implemented
+❌ Form Client Side Error Handling
+❌ Form Submission Process
+❌ Form Post Api Error Handling + ❌ Translation
+❌ Form Post Api Success Handling + ❌ Translation
+-->
+
+<template>
+	<section
+		class="container-fluid pt-container pb-container h-screen-inner min grid place-items-center"
+	>
+		<Transition mode="out-in" name="slide-up-down">
+			<Dialog v-if="dialog && dialog.type" :dialog="dialog" />
+		</Transition>
+	</section>
+</template>
+
+<script lang="ts">
+import { useI18n } from 'vue-i18n';
+
+definePageMeta({
+	layout: 'inner',
+});
+
+export default {
+	head: {
+		title: 'OAuth Login',
+	},
+	setup() {
+		const { t } = useI18n();
+
+		const router = useRouter();
+		const route = useRoute();
+
+		const dialog = <any>reactive({});
+		const config = useRuntimeConfig();
+
+		onMounted(async () => {
+			setLoading(true);
+			const [success, error] = await loginViaOAuthProvider({
+				code: route?.query?.code ?? '',
+				provider_id: route?.query?.state ?? '',
+				redirect_uri: `${config.BASE_WEB_URL}/oauth/callback`,
+			});
+			setLoading(false);
+
+			success ? successHandler(success) : errorHandler(error);
+		});
+
+		function successHandler(res: any) {
+			const register_token = res?.register_token ?? '';
+
+			if (!!register_token) {
+				router.push(`/auth/signup?register_token=${register_token}`);
+			} else {
+				setStates(res?.login ?? null);
+				router.push(`/profile`);
+			}
+		}
+
+		function errorHandler(res: any) {
+			Object.assign(dialog, {
+				type: 'error',
+				heading: 'Headings.UnableToOAuth',
+				body: `${t('Error.UnableToOAuth')}: ${res?.detail ?? ''}`,
+				primaryBtn: {
+					label: 'Links.GoBack',
+					onclick: () => {
+						router.push('/auth/login');
+					},
+				},
+				secondaryBtn: null,
+			});
+		}
+
+		return { dialog };
+	},
+};
+</script>
+
+<style scoped></style>
