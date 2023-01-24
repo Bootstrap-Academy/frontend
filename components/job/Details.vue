db51ad77cff8e3c44b6855273da176746c201cf5
Rumman-Fahim <rumman.fahim97@gmail.com>
2023-01-24 13:48:41 +0500

    added job details req

--- components/job/Details.vue
+++ components/job/Details.vue
@@ -56,7 +56,7 @@ export default defineComponent({
 
 			let updatedArr = arr.map((item) => {
 				return {
-					text: item[1],
+					text: `${item[1]} level ${item[2]}`,
 					link: `/skill-tree/${item[0]}?subSkillID=${item[1]}`,
 				};
 			});
