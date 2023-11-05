bca4eea61a3e1c851bcb8fce816a5ac42040c388
JohKei <kaebjohann@googlemail.com>
2023-11-05 13:39:13 +0100

    add skillTree types

--- composables/skilltree.ts
+++ composables/skilltree.ts
@@ -1,42 +1,46 @@
-import { ref } from 'vue';
+import { ref } from "vue";
+import { SkillTree, RootSkill, SubSkill } from "~/types/skillTreeTypes";
 
-export const useSubSkillTree = () => useState('subSkillTree', () => null);
-export const useRootSkillTree = () => useState('rootSkillTree', () => null);
+export const useSubSkillTree = () =>
+	useState<SkillTree<SubSkill>>(
+		"subSkillTree",
+		() => new SkillTree<SubSkill>()
+	);
+export const useRootSkillTree = () =>
+	useState<SkillTree<RootSkill>>(
+		"rootSkillTree",
+		() => new SkillTree<RootSkill>()
+	);
 
 export async function getRootSkillTree() {
 	try {
-		const response = await GET('/skills/skilltree');
+		const response: SkillTree<RootSkill> = await GET("/skills/skilltree");
 
 		const rootSkillTree = useRootSkillTree();
 		rootSkillTree.value = response;
 
-		return [response, null];
+		return response;
 	} catch (error: any) {
 		return [null, error.data];
 	}
 }
-
-
-
 
 export async function getSubSkillTree(id: string) {
 	try {
 		if (!!!id) {
-			throw { data: { detail: 'Missing sub skill id' } };
+			throw { data: { detail: "Missing sub skill id" } };
 		}
 
-		const response = await GET(`/skills/skilltree/${id}`);
+		const response: SkillTree<SubSkill> = await GET(`/skills/skilltree/${id}`);
 
 		const subSkillTree = useSubSkillTree();
 		subSkillTree.value = response;
 
-		return [response, null];
+		return response;
 	} catch (error: any) {
 		return [null, error.data];
 	}
 }
-
-
 
 export function createPathwaysForTree(map: any, nodes: any[], nodeSize: any) {
 	if (!!!map[0] || !!!map[0][0]) return [];
@@ -54,10 +58,10 @@ export function createPathwaysForTree(map: any, nodes: any[], nodeSize: any) {
 
 			let parentNodeRef = map[parentNode.row][parentNode.column];
 
-			let x1 = startNodeRef?.getAttribute('x') ?? 0;
-			let y1 = startNodeRef?.getAttribute('y') ?? 0;
-			let x2 = parentNodeRef?.getAttribute('x') ?? 0;
-			let y2 = parentNodeRef?.getAttribute('y') ?? 0;
+			let x1 = startNodeRef?.getAttribute("x") ?? 0;
+			let y1 = startNodeRef?.getAttribute("y") ?? 0;
+			let x2 = parentNodeRef?.getAttribute("x") ?? 0;
+			let y2 = parentNodeRef?.getAttribute("y") ?? 0;
 
 			let space = nodeSize * 0.5;
 
@@ -65,8 +69,9 @@ export function createPathwaysForTree(map: any, nodes: any[], nodeSize: any) {
 				arr.push({
 					node: node.id,
 					parent: parentNode.id,
-					path: `M${parseInt(x1) + space} ${parseInt(y1) + space} L${parseInt(x2) + space
-						} ${parseInt(y2) + space}`,
+					path: `M${parseInt(x1) + space} ${parseInt(y1) + space} L${
+						parseInt(x2) + space
+					} ${parseInt(y2) + space}`,
 				});
 			}
 		});
@@ -103,8 +108,8 @@ export function scrollMapToNode(
 		shiftBy = nodeSize * 0.5;
 	}
 
-	let cx = parseInt(ref.getAttribute('x') ?? 0) + shiftBy;
-	let cy = parseInt(ref.getAttribute('y') ?? 0) + shiftBy;
+	let cx = parseInt(ref.getAttribute("x") ?? 0) + shiftBy;
+	let cy = parseInt(ref.getAttribute("y") ?? 0) + shiftBy;
 	let screenCenterLeft = mapRef.clientWidth * 0.5;
 	let screenCenterTop = mapRef.clientHeight * 0.5;
 	let centerTop = cy - screenCenterTop;
@@ -113,6 +118,6 @@ export function scrollMapToNode(
 	mapRef.scroll({
 		top: centerTop,
 		left: centerLeft,
-		behavior: smooth ? 'smooth' : 'auto',
+		behavior: smooth ? "smooth" : "auto",
 	});
 }
