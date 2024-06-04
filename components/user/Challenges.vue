b0eb0b6455203b3f6b65c96cbc91859c4ed86696
Lauritz Tieste <lauritz6000000@gmail.com>
2024-06-04 14:53:21 +0200

    Redesign challenges statistics on profile page

--- components/user/Challenges.vue
+++ components/user/Challenges.vue
@@ -1,74 +1,57 @@
 <template>
   <section class="bg-secondary card style-card grid gap-card-sm">
-    <div class="flex flex-wrap gap-box items-start justify-between mb-2">
-      <SectionTitle
-        sub
-        heading="Headings.MyChallengesStats"
-        body="Body.MyChallengesStats"
-        class="!m-0"
-      />
-      <ChallengesMyRank :rank="myChallengesStats?.rank ?? 0" />
+    <div class="flex max-md:flex-wrap gap-box items-start justify-between mb-2">
+      <SectionTitle sub heading="Headings.MyChallengesStats" body="Body.MyChallengesStats" class="!m-0" />
+      <NuxtLink to="/challenges/leader-board" class="max-sm:hidden">
+        <Btn class="w-full justify-center" :icon="TrophyIcon">
+          {{ t("Buttons.GoToLeaderBoard") }}
+        </Btn>
+      </NuxtLink>
     </div>
+
+    <ChallengesProgress v-if="progress.total != null" :heading="t('Headings.Progress')" :subdivisions="[
+      { value: progress.attempted, color: 'bg-warning' },
+      { value: progress.solved, color: 'bg-accent' },
+    ]" :total="progress.total" />
+    <p v-else class="px-2 max-w-max rounded-md text-error border border-error">
+      {{ t("Headings.NoChallengesStats") }}
+    </p>
 
     <ChallengesProgressSummary :data="codingChallengesStats ?? null" />
 
-    <ChallengesProgress
-      v-for="(item, i) of progress"
-      :key="i"
-      :data="item"
-      :loading="loading"
-    />
-
-    <article class="flex flex-wrap gap-card-sm mt-4">
-      <!-- <NuxtLink to="/profile/challenges">
-				<Btn secondary>
-					{{ t('Buttons.MyChallenges') }}
-				</Btn>
-			</NuxtLink> -->
-      <NuxtLink to="/challenges/leader-board">
-        <Btn>
-          {{ t("Buttons.LeaderBoard") }}
-        </Btn>
-      </NuxtLink>
-    </article>
+    <NuxtLink to="/challenges/leader-board" class="sm:hidden mt-4">
+      <Btn class="w-full justify-center">
+        {{ t("Buttons.GoToLeaderBoard") }}
+      </Btn>
+    </NuxtLink>
   </section>
 </template>
 
 <script lang="ts">
+import { TrophyIcon } from "@heroicons/vue/24/outline";
 import { defineComponent } from "vue";
 import { useI18n } from "vue-i18n";
 
 export default defineComponent({
+  components: { TrophyIcon },
   setup() {
     const { t } = useI18n();
-
-    const myChallengesStats = useMyChallengesStats();
-
+    
     const codingChallengesStats: any = useCodingChallengesStats();
-    const loading = ref(!!!codingChallengesStats.value);
+
     onMounted(async () => {
       await getCodingChallengesStats();
-      loading.value = false;
-    });
-    // const propgress = computed(() => {});
-    const progress = computed(() => {
-      return [
-        {
-          heading: t("Headings.Attempted"),
-          current: codingChallengesStats.value?.attempted ?? 0,
-          total: codingChallengesStats.value?.total ?? 10,
-        },
-        {
-          heading: t("Headings.ChallengesSolved"),
-          current: codingChallengesStats.value?.solved ?? 0,
-          total: codingChallengesStats.value?.total ?? 10,
-        },
-      ];
     });
 
-    return { t, loading, myChallengesStats, codingChallengesStats, progress };
+    const progress = computed(() => {
+      return {
+        attempted: codingChallengesStats.value?.attempted ?? 0,
+        solved: codingChallengesStats.value?.solved ?? 0,
+        total: codingChallengesStats.value?.total,
+      };
+    });
+
+    return { t, codingChallengesStats, progress, TrophyIcon };
   },
 });
 </script>
-
-<style scoped></style>
