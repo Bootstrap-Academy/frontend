005166a3bb2e333948b9c94dd01999a2fd3a654d
PeachpitRipley <andrea_kuellmer@web.de>
2025-10-30 10:25:08 +0100

    chore(eslint): simplify lint config and scope (#144)

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,7 @@
-const {
-  defineConfig,
-} = require("eslint/config");
-
+const { defineConfig } = require("eslint/config");
 const parser = require("vue-eslint-parser");
 const js = require("@eslint/js");
-
-const {
-  FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
@@ -16,18 +10,27 @@ const compat = new FlatCompat({
 });
 
 module.exports = defineConfig([{
+  // ignore global files
+  ignores: [
+    "**/.output/**",
+    "**/.nuxt/**",
+    "**/dist/**",
+    "**/coverage/**",
+    "**/node_modules/**"
+  ],
+},
+// ignore specific files
+{ files: ["**/*.{js,ts,vue}"],
   extends: compat.extends(),
-
-  rules: {
-    "indent": ["error", 2],
-  },
-
   languageOptions: {
     parser: parser,
     "sourceType": "module",
-
     parserOptions: {
       "parser": "@typescript-eslint/parser",
     },
   },
-}]);
+  rules: {
+    "indent": ["error", 2],
+  },
+}
+]);
